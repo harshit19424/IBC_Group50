64ca7c604fe57528c128b237d6c014130f81aecc
harshit19424 <harshit19424@iiitd.ac.in>
2021-12-14 23:23:05 +0530

    final

---
+++ src/constants/constant.js
@@ -0,0 +1,86 @@
+export const CARD_ARRAY = [
+    {
+        name: 'naruto',
+        img: '/images/naruto.jfif'
+    },
+    {
+        name: 'itachi',
+        img: '/images/itachi.png'
+    },
+    {
+        name: 'gaara',
+        img: '/images/Gaara.png'
+    },
+    {
+        name: 'jiraya',
+        img: '/images/jiraya.png'
+    },
+    {
+        name: 'kakashi',
+        img: '/images/Kakashi_Hatake.png'
+    },
+    {
+        name: 'madara',
+        img: '/images/madara.png'
+    },
+    {
+        name: 'naruto',
+        img: '/images/naruto.jfif'
+    },
+    {
+        name: 'itachi',
+        img: '/images/itachi.png'
+    },
+    {
+        name: 'gaara',
+        img: '/images/Gaara.png'
+    },
+    {
+        name: 'jiraya',
+        img: '/images/jiraya.png'
+    },
+    {
+        name: 'kakashi',
+        img: '/images/Kakashi_Hatake.png'
+    },
+    {
+        name: 'madara',
+        img: '/images/madara.png'
+    },
+    {
+        name: 'sakura',
+        img: '/images/sakura.png'
+    },
+    {
+        name: 'sakura',
+        img: '/images/sakura.png'
+    },
+    {
+        name: 'rock',
+        img: '/images/rock.png'
+    },
+    {
+        name: 'rock',
+        img: '/images/rock.png'
+    },
+
+]
+
+export const CARD_STATE = [
+    {
+        name: 'white',
+        img: '/images/white.png'
+    },
+    {
+        name: 'blank',
+        img: '/images/blank.jpg'
+    }
+]
+
+export const misc = {
+    HEADING: "NFT Matchmaking",
+    ACCOUNT: "Account: ",
+    HEADER: "Start Matching!",
+    LIVES: "Lives: ",
+    TOKENS: "Tokens Collected: ",
+}
